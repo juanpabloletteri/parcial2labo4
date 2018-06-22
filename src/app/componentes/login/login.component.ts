6090c6120c94aea1f7af53cb99bd0388aad2c153
juanpabloletteri <pabloletteri@gmail.com>
2018-06-22 00:06:32 -0300

    ingreso html

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Usuario } from '../../clases/usuario';
+import { UsuarioService } from '../../servicios/usuario.service';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userform: FormGroup;
+
+  constructor(private fb: FormBuilder, private miUsuario: Usuario, private miServicioUsuario: UsuarioService, public rute: Router) { }
 
   ngOnInit() {
+    this.userform = this.fb.group({
+      'mail': new FormControl('', Validators.required),
+      'password': new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit(value: string) {
+    this.miUsuario.mail = this.userform.value.mail;
+    this.miUsuario.password = this.userform.value.password;
+    alert("hacer logica de login");
   }
 
 }
