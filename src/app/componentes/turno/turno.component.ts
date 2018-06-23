41a743134537d8efb020a931ab4449b0e590bbb5
juanpabloletteri <pabloletteri@gmail.com>
2018-06-23 20:18:20 -0300

    modificaciones a calendario

--- src/app/componentes/turno/turno.component.ts
+++ src/app/componentes/turno/turno.component.ts
@@ -15,9 +15,36 @@ export class TurnoComponent implements OnInit {
   @Input() unaMascota: Mascota;
   date: Date;
   observaciones: string;
+  invalidDates: Array<Date>;
+  es: any;
+
   constructor(private miTurno: Turno, private miServicioTurno: TurnoService, private rute: Router) { }
 
   ngOnInit() {
+    this.es = {
+      firstDayOfWeek: 1,
+      dayNames: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
+      dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+      dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+      monthNames: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+      monthNamesShort: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
+      today: 'Hoy',
+      clear: 'Borrar'
+    }
+
+
+
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let prevMonth = (month === 0) ? 11 : month - 1;
+    let prevYear = (prevMonth === 11) ? year - 1 : year;
+    let nextMonth = (month === 11) ? 0 : month + 1;
+    let nextYear = (nextMonth === 0) ? year + 1 : year;
+
+    let invalidDate = new Date();
+    invalidDate.setDate(today.getDate() - 1);
+    this.invalidDates = [today, invalidDate];
   }
 
   confirmarTurno() {
